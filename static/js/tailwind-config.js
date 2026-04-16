e75ce75c005a8762a8b9bcfc9d32b46549493f52
Kandel-Bibas <bibas1xy@gmail.com>
2026-04-16 15:17:07 -0500

    feat(ui): base layout + design tokens from mockups
    Co-authored-by: Abhishek Ray <104149715+abhishekray-edu@users.noreply.github.com>

---
+++ static/js/tailwind-config.js
@@ -0,0 +1,67 @@
+tailwind.config = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "secondary": "#bcc7de",
+        "on-error-container": "#ffdad6",
+        "surface-variant": "#2d3449",
+        "primary-fixed-dim": "#4edea3",
+        "primary-fixed": "#6ffbbe",
+        "error": "#ffb4ab",
+        "surface-tint": "#4edea3",
+        "primary": "#4edea3",
+        "on-error": "#690005",
+        "surface-container-high": "#222a3d",
+        "outline": "#8e9196",
+        "on-secondary": "#263143",
+        "secondary-container": "#3e495d",
+        "on-tertiary-fixed": "#001e2c",
+        "surface-container-low": "#131b2e",
+        "surface-container": "#171f33",
+        "on-background": "#dae2fd",
+        "tertiary-container": "#001d2a",
+        "tertiary-fixed": "#c4e7ff",
+        "on-surface-variant": "#c4c6cc",
+        "surface-container-highest": "#2d3449",
+        "on-primary": "#003824",
+        "tertiary": "#7bd0ff",
+        "on-primary-fixed-variant": "#005236",
+        "surface-dim": "#0b1326",
+        "on-tertiary-fixed-variant": "#004c69",
+        "error-container": "#93000a",
+        "surface-container-lowest": "#060e20",
+        "secondary-fixed": "#d8e3fb",
+        "on-secondary-fixed-variant": "#3c475a",
+        "on-primary-container": "#009567",
+        "on-tertiary-container": "#008cbd",
+        "outline-variant": "#44474c",
+        "inverse-primary": "#006c49",
+        "primary-container": "#001f12",
+        "on-primary-fixed": "#002113",
+        "surface-bright": "#31394d",
+        "on-secondary-fixed": "#111c2d",
+        "secondary-fixed-dim": "#bcc7de",
+        "inverse-on-surface": "#283044",
+        "background": "#0b1326",
+        "on-secondary-container": "#aeb9d0",
+        "on-tertiary": "#00354a",
+        "surface": "#0b1326",
+        "on-surface": "#dae2fd",
+        "tertiary-fixed-dim": "#7bd0ff",
+        "inverse-surface": "#dae2fd"
+      },
+      borderRadius: {
+        "DEFAULT": "0.125rem",
+        "lg": "0.25rem",
+        "xl": "0.5rem",
+        "full": "0.75rem"
+      },
+      fontFamily: {
+        "headline": ["Manrope"],
+        "body": ["Inter"],
+        "label": ["Inter"]
+      }
+    }
+  }
+};
